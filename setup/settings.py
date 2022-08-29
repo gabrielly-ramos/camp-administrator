e36d5275de2f542cf593e3b8d36581f887683130
gabrielly-ramos <gabrielly.ramos+git@a55.tech>
2022-08-29 00:48:24 -0300

    add docker

--- setup/settings.py
+++ setup/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'PASSWORD': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
