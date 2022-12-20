a386b69df9ce686153f4e9eecf3934a680e7a767
Gabrielly Ramos <gabrielly.ramos+git@a55.tech>
2022-12-20 12:01:44 -0300

    remove asset

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn setup.wsgi:application --log-file - --log-level debug
-release: python manage.py migrate
--- setup/settings.py
+++ setup/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'app',
         'PASSWORD': 'postgres',
         'USER': 'postgres',
         'HOST': 'db',
